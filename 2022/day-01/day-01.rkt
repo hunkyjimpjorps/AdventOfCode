92bb2cb2ea5c3f75ce1fdefafdf44e9f3cece6d8
Hunky Jimpjorps <thechairman@thechairman.info>
2022-12-01 00:14:27 -0500

    day 1 complet

--- 2022/day-01/day-01.rkt
+++ 2022/day-01/day-01.rkt
@@ -1,6 +1,20 @@
 #lang racket
 
 (require advent-of-code
-         threading
-         seq)
+         threading)
 
+(define calorie-data (fetch-aoc-input (find-session) 2022 1))
+
+;; part 1
+(~> calorie-data
+    (string-split "\n\n")
+    (map (λ~> string-split (map string->number _) (apply + _)) _)
+    (apply max _))
+
+;; part 2
+(~> calorie-data
+    (string-split "\n\n")
+    (map (λ~> string-split (map string->number _) (apply + _)) _)
+    (sort _ >)
+    (take 3)
+    (apply + _))
