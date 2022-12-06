f5cd4dfa7d3bcdfcc18312d2ca55a476dca956b5
Hunky Jimpjorps <thechairman@thechairman.info>
2022-12-06 12:59:21 -0500

    day 6 cleanup

--- 2022/day-06/day-06.rkt
+++ 2022/day-06/day-06.rkt
@@ -1,20 +1,19 @@
 #lang racket
 
 (require advent-of-code
-         threading
          (only-in relation ->list ->set)
          (only-in algorithms sliding))
 
-(define buffer (~> (fetch-aoc-input (find-session) 2022 6)))
+(define buffer (fetch-aoc-input (find-session) 2022 6))
 
 (define (find-marker data type)
   (define n
-    (match type
-      ['start-of-packet 4]
-      ['start-of-message 14]))
+    (case type
+      [(start-of-packet) 4]
+      [(start-of-message) 14]))
   (for/first ([chunk (in-list (sliding (->list data) n))]
               [i (in-naturals n)]
-              #:when (= n (~> chunk remove-duplicates length)))
+              #:unless (check-duplicates chunk))
     i))
 
 ;; part 1
