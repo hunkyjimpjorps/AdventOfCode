7d83fb6a52dfbb5b96b9d2de6b9e06dd78e60faa
Hunky Jimpjorps <thechairman@thechairman.info>
2022-12-21 10:27:48 -0500

    day 21 cleanup

--- 2022/day-21/day-21.rkt
+++ 2022/day-21/day-21.rkt
@@ -19,33 +19,23 @@
   (for/hash ([m raw-monkeys] #:do [(match-define (monkey name op) (parse-monkey m))])
     (values name op)))
 
-(define (parse f name1 name2)
+;; part 1
+(define (evaluate-monkey m-name [guess #f])
+  (match-define (op f name1 name2)
+    (if (and guess (equal? m-name "humn"))
+        (op 'constant guess #f)
+        (hash-ref monkey-table m-name)))
   (match f
     ['constant name1]
-    ['+ (+ (evaluate-monkey name1) (evaluate-monkey name2))]
-    ['- (- (evaluate-monkey name1) (evaluate-monkey name2))]
-    ['* (* (evaluate-monkey name1) (evaluate-monkey name2))]
-    ['/ (/ (evaluate-monkey name1) (evaluate-monkey name2))]))
-
-;; part 1
-(define (evaluate-monkey m-name)
-  (match-define (op f name1 name2) (hash-ref monkey-table m-name))
-  (parse f name1 name2))
+    ['+ (+ (evaluate-monkey name1 guess) (evaluate-monkey name2 guess))]
+    ['- (- (evaluate-monkey name1 guess) (evaluate-monkey name2 guess))]
+    ['* (* (evaluate-monkey name1 guess) (evaluate-monkey name2 guess))]
+    ['/ (/ (evaluate-monkey name1 guess) (evaluate-monkey name2 guess))]))
 
 (evaluate-monkey "root")
 
 ;; part 2
 (match-define (op _ branch-1 branch-2) (hash-ref monkey-table "root"))
-
-(define (evaluate-monkey* m-name guess)
-  (match-define (op f name1 name2)
-    (if (equal? m-name "humn") (op 'constant guess #f) (hash-ref monkey-table m-name)))
-  (match f
-    ['constant name1]
-    ['+ (+ (evaluate-monkey* name1 guess) (evaluate-monkey* name2 guess))]
-    ['- (- (evaluate-monkey* name1 guess) (evaluate-monkey* name2 guess))]
-    ['* (* (evaluate-monkey* name1 guess) (evaluate-monkey* name2 guess))]
-    ['/ (/ (evaluate-monkey* name1 guess) (evaluate-monkey* name2 guess))]))
 
 ;; the branch that doesn't depend on humn
 (define result-2 (evaluate-monkey branch-2))
@@ -53,6 +43,6 @@
 ;; I plugged in numbers for guess to find a suitable starting range and settled on the first one
 ;; that I found that got me within a million
 (for/first ([guess (in-naturals 3059361690000)]
-            #:do [(define result-1 (evaluate-monkey* branch-1 guess))]
+            #:do [(define result-1 (evaluate-monkey branch-1 guess))]
             #:when (= result-1 result-2))
   guess)
