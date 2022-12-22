19db5e23c743f6dcca1f6c982f590a8cb948a126
HJ <thechairman@thechairman.info>
2022-12-21 23:30:01 -0500

    2020 day 16 progress

--- 2020/day-16/day-16.rkt
+++ 2020/day-16/day-16.rkt
@@ -1,6 +1,8 @@
 #lang racket
 
-(require advent-of-code
+(require racket/struct
+         advent-of-code
+         fancy-app
          relation
          threading
          rebellion/base/range)
@@ -22,13 +24,29 @@
 (match-define (list (app (λ~>> make-lines (map process-rules)) ticket-rules)
                     (app (λ~>> make-lines second seperate-fields) your-ticket)
                     (app (λ~>> make-lines rest (map seperate-fields)) other-tickets))
-  (~> (fetch-aoc-input (find-session) 2020 16) (string-split "\n\n")))
+  (~> (fetch-aoc-input (find-session) 2020 16 #:cache #true) (string-split "\n\n")))
 
-(define (ticket-scanning-error-rate ticket rules)
-  (for/sum ([ticket-field (in-list ticket)]
-            [ticket-rule (in-list rules)]
-            #:do [(match-define (field-rule _ r1 r2) ticket-rule)]
-            #:unless (or (range-contains? r1 ticket-field) (range-contains? r2 ticket-field)))
-           ticket-field))
+;; part 1
+(define (fails-all-checks? field rules)
+  (define rule-list (~>> rules (map (λ~> struct->list rest)) flatten))
+  (for/and ([rule (in-list rule-list)])
+    (not (range-contains? rule field))))
 
-(for/sum ([ticket (in-list other-tickets)]) (ticket-scanning-error-rate ticket ticket-rules))
+(define (ticket-scanning-error-rate tickets rules)
+  (for*/sum
+   ([ticket (in-list tickets)] (field (in-list ticket)) #:when (fails-all-checks? field rules))
+   field))
+
+(ticket-scanning-error-rate other-tickets ticket-rules)
+
+;; part 2
+(define valid-tickets (filter (ormap (fails-all-checks? _ ticket-rules) _) other-tickets))
+
+(define fields (apply map list valid-tickets))
+
+(for/list ([field (in-list fields)])
+  (for*/list (
+              [rule (in-list ticket-rules)]
+              #:unless (not (or (range-contains? (field-rule-range1 rule) value)
+                                (range-contains? (field-rule-range2 rule) value))))
+    (field-rule-name rule)))
