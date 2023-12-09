7f3c46c9e1aca82cc0a4355e906eac140df26a61
HJ <thechairman@thechairman.info>
2023-12-09 10:05:41 -0500

    day 9 alternate racket solution

---
+++ aoc2023-other/day-09/day-09-polynomial.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+(require advent-of-code
+         threading
+         simple-polynomial/tools)
+
+(define histories
+  (for/list ([raw-history (in-lines (open-aoc-input (find-session) 2023 9 #:cache #true))])
+    (~>> raw-history string-split (map string->number))))
+
+(for/lists (left right #:result (cons (apply + left) (apply + right)))([history (in-list histories)])
+  (define f (interpolate-at-integer-points history))
+  (values (f -1) (f (length history))))
