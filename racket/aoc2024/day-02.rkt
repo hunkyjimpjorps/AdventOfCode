57e8355bd0d13614fa4ee93fc1da75ee059a0f4c
H.J <thechairman@thechairman.info>
2024-12-03 15:46:23 -0500

    racket 2024 day 2 complete

--- racket/aoc2024/day-02.rkt
+++ racket/aoc2024/day-02.rkt
@@ -7,21 +7,20 @@
     (for/list ([n (in-list (string-split line))])
       (string->number n))))
 
-(define (get-deltas xs)
-  (for/list ([a (in-list xs)]
-             [b (in-list (rest xs))])
-    (- b a)))
-
-(define (safe? deltas)
+(define (safe? report)
+  (define deltas
+    (for/list ([a (in-list report)]
+               [b (in-list (rest report))])
+      (- b a)))
   (for/or ([ok-values '((1 2 3) (-1 -2 -3))])
     (for/and ([d (in-list deltas)])
       (member d ok-values))))
 
 ;; part 1
-(for/sum ([report (in-list reports)] #:when (safe? (get-deltas report))) 1)
+(for/sum ([report (in-list reports)] #:when (safe? report)) 1)
 
 ;; part 2
 (for/sum ([report (in-list reports)] ;
           #:when (for/or ([alt (in-combinations report (sub1 (length report)))])
-                   (safe? (get-deltas alt))))
+                   (safe? alt)))
          1)
