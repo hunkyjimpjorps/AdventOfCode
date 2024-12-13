bf6c64b518cadee33bf3f0f3e346cf4bdca43e14
H.J <thechairman@thechairman.info>
2024-12-13 10:08:18 -0500

    racket/rosette 2024 day 13

---
+++ racket/aoc2024/day-13-rosette.rkt
@@ -0,0 +1,46 @@
+#lang rosette
+
+(require advent-of-code
+         threading)
+
+(struct Machine (a b prize) #:transparent)
+(struct Dist (x y) #:transparent)
+
+(define button-re #px"Button .: X\\+(\\d+), Y\\+(\\d+)")
+(define prize-re #px"Prize: X=(\\d+), Y=(\\d+)")
+
+(define MACHINES
+  (for/list ([machine (string-split (fetch-aoc-input (find-session) 2024 13 #:cache #t) "\n\n")])
+    (match-define (list a b prize) (string-split machine "\n"))
+
+    (match-define (list (list (app string->number ax) (app string->number ay)))
+      (regexp-match* button-re a #:match-select cdr))
+    (match-define (list (list (app string->number bx) (app string->number by)))
+      (regexp-match* button-re b #:match-select cdr))
+    (match-define (list (list (app string->number px) (app string->number py)))
+      (regexp-match* prize-re prize #:match-select cdr))
+
+    (Machine (Dist ax ay) (Dist bx by) (Dist px py))))
+
+(define (find-price machine)
+  (match-define (Machine (Dist ax ay) (Dist bx by) (Dist px py)) machine)
+  (define-symbolic a b integer?)
+  (define sol
+    (solve (begin
+             (assert (positive? a))
+             (assert (positive? b))
+             (assert (= px (+ (* ax a) (* bx b))))
+             (assert (= py (+ (* ay a) (* by b)))))))
+  (cond
+    [(unsat? sol) 0]
+    [else (evaluate (+ (* 3 a) b) sol)]))
+
+;; part 1
+(for/sum ([machine (in-list MACHINES)]) (find-price machine))
+
+;; part 2
+(define/match (recalibrate _machine)
+  [((Machine a b (Dist prize-x prize-y)))
+   (Machine a b (Dist (+ 10000000000000 prize-x) (+ 10000000000000 prize-y)))])
+
+(for/sum ([machine (in-list MACHINES)]) (~> machine recalibrate find-price))
