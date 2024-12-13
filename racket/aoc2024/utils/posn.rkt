8fa35dc60e60c11deed938ad3b00e92db88b6135
H.J <thechairman@thechairman.info>
2024-12-12 23:52:36 -0500

    more utility functions

--- racket/aoc2024/utils/posn.rkt
+++ racket/aoc2024/utils/posn.rkt
@@ -1,10 +1,13 @@
 #lang racket
 
-(provide Posn
+(provide (struct-out Posn)
          string->posn-grid
          posn-add
          posn-diff
+         next-row
+         next-col
          in-cardinal-directions
+         in-diagonal-directions
          in-all-directions
          neighbors-of)
 
@@ -20,7 +23,13 @@
 (define (posn-diff a b)
   (Posn (- (Posn-x a) (Posn-x b)) (- (Posn-y a) (Posn-y b))))
 
+(define/match (next-row _)
+  [((Posn x y)) (Posn (add1 x) 0)])
+(define/match (next-col _)
+  [((Posn x y)) (Posn x (add1 y))])
+
 (define in-cardinal-directions (list (Posn 1 0) (Posn -1 0) (Posn 0 1) (Posn 0 -1)))
+(define in-diagonal-directions (list (Posn 1 1) (Posn 1 -1) (Posn -1 -1) (Posn -1 1)))
 (define in-all-directions
   (list (Posn -1 0)
         (Posn -1 1)
