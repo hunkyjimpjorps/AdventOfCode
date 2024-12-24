78f7a0f6967e8582a996c42b3d125a7f43a307d4
H.J <thechairman@thechairman.info>
2024-12-23 19:02:42 -0500

    racket 2024 day 23 complete

--- racket/aoc2024/day-23.rkt
+++ racket/aoc2024/day-23.rkt
@@ -4,40 +4,6 @@
          graph
          threading)
 
-(define test-input
-  "kh-tc
-qp-kh
-de-cg
-ka-co
-yn-aq
-qp-ub
-cg-tb
-vc-aq
-tb-ka
-wh-tc
-yn-cg
-kh-ub
-ta-co
-de-co
-tc-td
-tb-wq
-wh-td
-ta-ka
-td-qp
-aq-cg
-wq-ub
-ub-vc
-de-ta
-wq-aq
-wq-vc
-wh-yn
-ka-de
-kh-ta
-co-tc
-wh-qp
-tb-vc
-td-yn")
-
 (define graph
   (~> (for/list (; (in-list (string-split test-input))
                  [line (in-lines (open-aoc-input (find-session) 2024 23 #:cache #true))])
