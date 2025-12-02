68e399d4a18b7d6a48e1c712b5b53cab84cac46e
H.J. <thechairman@thechairman.info>
2025-12-02 12:29:54 -0500

    racket 2025 day 2 complete

--- racket/aoc2025/day-02.rkt
+++ racket/aoc2025/day-02.rkt
@@ -8,13 +8,32 @@
 
 (define id-ranges
   (for/range-set
-   #:comparator real<=>
+   #:comparator natural<=>
    ([rng (~> (fetch-aoc-input (find-session) 2025 2 #:cache #true) (string-trim) (string-split ","))])
-   (~> rng (string-split "-") (map string->number _) (apply closed-range _))))
+   (~> rng (string-split "-") (map string->number _) (apply closed-range _ #:comparator natural<=>))))
+
+(define (repeat n times)
+  (define shift (expt 10 (add1 (order-of-magnitude n))))
+  (define (do-repeat acc t)
+    (cond
+      [(= 1 t) acc]
+      [else (do-repeat (+ n (* acc shift)) (sub1 t))]))
+  (do-repeat n times))
 
 ;; part 1
+
 (for/sum ([n (in-inclusive-range 1 99999)] ;
-          #:do [(define len (inexact->exact (ceiling (log n 10))))]
-          #:do [(define nn (+ n (* n (expt 10 len))))]
+          #:do [(define nn (repeat n 2))]
           #:when (range-set-contains? id-ranges nn))
          nn)
+
+;; part 2
+(define additional-nonsense
+  (apply set-union
+         (for/list ([len (in-inclusive-range 1 5)]
+                    [times (in-list '(10 5 3 2 2))])
+           (for*/set ([l (in-range (expt 10 (sub1 len)) (expt 10 len))]
+                      [t (in-inclusive-range 2 times)])
+             (repeat l t)))))
+
+(for/sum ([n (in-set additional-nonsense)] #:when (range-set-contains? id-ranges n)) n)
