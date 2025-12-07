523ec3cc7912a3f904e7e3f09a2ec74793c44514
H.J. <thechairman@thechairman.info>
2025-12-06 22:47:22 -0500

    racket 2025 day 6 tweaking

--- racket/aoc2025/day-06.rkt
+++ racket/aoc2025/day-06.rkt
@@ -11,22 +11,23 @@
   [("+") +]
   [("*") *])
 
-(for/sum
- ([col
-   (in-list
-    (~> input (string-split "\n") reverse (map (λ~> (string-split " " #:repeat? #t)) _) transpose))])
- (apply (f (first col)) (~> col rest (map string->number _))))
+(define (math1 cols [acc 0])
+  (match cols
+    [(list) acc]
+    [(list* (list* op xs) rest) (math1 rest (+ acc (apply (f op) (map string->number xs))))]))
+
+(~> input (string-split "\n") reverse (map (λ~> (string-split " " #:repeat? #t)) _) transpose math1)
 
 ;;part 2
 (define (undigits strs)
   (~> strs (apply string _) (string-trim " " #:repeat? #t) string->number))
 
-(for/fold ([acc 0]
-           [register '()]
-           [op #f]
-           #:result (+ acc (apply (f op) register)))
-          ([col (~> input (string-split "\n") (map string->list _) transpose)])
-  (match col
-    [(list #\space ...) (values (+ acc (apply (f op) register)) '() #f)]
-    [(list ch ... #\space) (values acc (cons (undigits ch) register) op)]
-    [(list ch ... new-op) (values acc (cons (undigits ch) register) (string new-op))]))
+(define (math2 cols [acc 0] [register '()] [op #f])
+  (match cols
+    [(list) (+ acc (apply (f op) register))]
+    [(list* (list #\space ...) rest) (math2 rest (+ acc (apply (f op) register)) '() #f)]
+    [(list* (list ch ... #\space) rest) (math2 rest acc (cons (undigits ch) register) op)]
+    [(list* (list ch ... new-op) rest)
+     (math2 rest acc (cons (undigits ch) register) (string new-op))]))
+
+(~> input (string-split "\n") (map string->list _) transpose math2)
