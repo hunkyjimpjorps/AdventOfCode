83bac14e8421082f64894025ffb954b3804e7cc7
H.J. <thechairman@thechairman.info>
2025-12-09 13:30:59 -0500

    racket 2025 day 9 cleanup

--- racket/aoc2025/day-09.rkt
+++ racket/aoc2025/day-09.rkt
@@ -22,16 +22,17 @@
 (define poly-sides (sliding (cons (last red-tiles) red-tiles) 2))
 
 (define (rectangle-hits-polygon? rect)
-  (match-define (list (Posn x1 y1) (Posn x2 y2)) rect)
-  (match-define (list rect-min-x rect-max-x) (sort (list x1 x2) <))
-  (match-define (list rect-min-y rect-max-y) (sort (list y1 y2) <))
-
-  (for/or ([side (in-list poly-sides)])
-    (match-define (list (Posn px1 py1) (Posn px2 py2)) side)
-    (not (or (<= (max px1 px2) rect-min-x)
-             (>= (min px1 px2) rect-max-x)
-             (<= (max py1 py2) rect-min-y)
-             (>= (min py1 py2) rect-max-y)))))
+  (match-let* ([(list (Posn x1 y1) (Posn x2 y2)) rect]
+               [(list rect-min-x rect-max-x) (sort (list x1 x2) <)]
+               [(list rect-min-y rect-max-y) (sort (list y1 y2) <)])
+    (for/or ([side (in-list poly-sides)])
+      (match-let* ([(list (Posn px1 py1) (Posn px2 py2)) side]
+                   [side-max-x (max px1 px2)] [side-min-x (min px1 px2)]
+                   [side-max-y (max py1 py2)] [side-min-y (min py1 py2)])
+        (not (or (<= side-max-x rect-min-x)
+                 (>= side-min-x rect-max-x)
+                 (<= side-max-y rect-min-y)
+                 (>= side-min-y rect-max-y)))))))
 
 (for/first ([rect (in-list rectangles)]
             #:unless (rectangle-hits-polygon? rect))
