22a78fd900959a9f592e535c971e0e90785b4006
H.J. <thechairman@thechairman.info>
2025-12-10 14:45:38 -0500

    racket 2025 day 10 complete

--- racket/aoc2025/day-10.rkt
+++ racket/aoc2025/day-10.rkt
@@ -21,36 +21,29 @@
 
 ;; part 1
 
-(define (minimize-lights machine)
-  (match-define (Machine lights buttons _) machine)
-  (for*/first ([n (in-naturals)]
-               [bs (in-combinations buttons n)]
-               #:when (set-empty? (foldl set-symmetric-difference lights bs)))
-    (length bs)))
-
-(~>> machines (map minimize-lights) (apply +))
+(for/sum ([machine (in-list machines)])
+         (match-define (Machine lights buttons _) machine)
+         (for*/first ([n (in-naturals)]
+                      [bs (in-combinations buttons n)]
+                      #:when (set-empty? (foldl set-symmetric-difference lights bs)))
+           (length bs)))
 
 ;; part 2
 
-(define (minimize-presses machine)
-  (clear-vc!)
-  (match-define (Machine _ buttons joltages) machine)
-  (define-symbolic* n integer? #:length (length buttons))
-
-  (define sol
-    (optimize #:minimize (list (foldl + 0 n))
-              #:guarantee (begin
-                            (for ([v (in-list n)])
-                              (assert (>= v 0)))
-                            (for ([total (in-list joltages)]
-                                  [index (in-naturals)])
-                              (define valid-n
-                                (for/list ([b (in-list buttons)]
-                                           [var (in-list n)]
-                                           #:when (set-member? b index))
-                                  var))
-                              (assert (= total (foldl + 0 valid-n)))))))
-
-  (evaluate (foldl + 0 n) sol))
-
-(~>> machines (map minimize-presses) (apply +))
+(for/sum ([machine (in-list machines)])
+         (match-define (Machine _ buttons joltages) machine)
+         (define-symbolic* n integer? #:length (length buttons))
+         (define sol
+           (optimize #:minimize (list (foldl + 0 n))
+                     #:guarantee (begin
+                                   (for ([v (in-list n)])
+                                     (assert (>= v 0)))
+                                   (for ([total (in-list joltages)]
+                                         [index (in-naturals)])
+                                     (define valid-n
+                                       (for/list ([b (in-list buttons)]
+                                                  [var (in-list n)]
+                                                  #:when (set-member? b index))
+                                         var))
+                                     (assert (= total (foldl + 0 valid-n)))))))
+         (evaluate (foldl + 0 n) sol))
